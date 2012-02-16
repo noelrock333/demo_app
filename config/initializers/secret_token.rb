0b45a0aec95ca57888309db52911bc101bf745f3
darkcn <darkcn@darkcn-rz.(none)>
2012-02-16 15:47:24 -0600

    Hola mundo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '98f24413ed57148f899d440364e0f01ac0155d7264a4a905f9b12829aa9816e93ae4fe68f2255e180656b93891876f152b70e338726334cabb4fce90cb02ed91'
